33c4d3a5c3838dc6e4859649f5b6847dad3ac823
divyanshuverma01 <coderverma129@gmail.com>
2026-07-07 01:14:56 +0530

    Time: 11 ms (52.43%) | Memory: 21.1 MB (30.09%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int checkPalindrome(string s,int i,int j){
+        int count=0;
+        while(i>=0 && j<s.length() && s[i]==s[j] ){
+            count++;
+            i--;
+            j++;
+            
+            
+        }
+        return count;
+    }
+    int countSubstrings(string s) {
+        int totalCount=0;
+        for(int center=0;center<s.length();center++){
+            int oddPalindrome=checkPalindrome(s,center,center);
+            int evenPalindrome=checkPalindrome(s,center,center+1);
+
+            totalCount=totalCount+oddPalindrome+evenPalindrome;
+        }
+        return totalCount;
+    }
+};
