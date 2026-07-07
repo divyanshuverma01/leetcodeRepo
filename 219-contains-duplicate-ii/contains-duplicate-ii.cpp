01715cfccbd329020bc79ccfc5a110800fc2fd66
divyanshuverma01 <coderverma129@gmail.com>
2026-07-08 04:56:22 +0530

    Time: 71 ms (74.18%) | Memory: 98.8 MB (13.88%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        map<int,int>mpp;
+        unordered_map<int,int>mpp;
         for (int i=0;i<nums.size();i++){
             if (mpp.find(nums[i])!=mpp.end()){
                 
