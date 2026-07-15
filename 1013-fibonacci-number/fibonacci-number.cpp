53e0d59770bb95ae0ce2d773af1f78fdcf2e4091
divyanshuverma01 <coderverma129@gmail.com>
2026-07-15 20:09:45 +0530

    Time: 3 ms (65.73%) | Memory: 8.4 MB (11.02%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,8 +1,23 @@
 class Solution {
 public:
-    int fib(int n) {
+    // int usingRecursion(int n){
+    //     if(n==0 || n==1) return n;
+    //     return fib(n-1)+fib(n-2);
+        
+    // }
+    int topDown(int n,vector<int>&dp){
         if(n==0 || n==1) return n;
-        int ans =fib(n-1)+fib(n-2);
+        if(dp[n]!=-1)return dp[n];
+        dp[n]=topDown(n-1,dp)+topDown(n-2,dp);
+        return dp[n];
+
+
+    }
+    int fib(int n) {
+        vector<int> dp(n+1,-1);
+        // int ans=usingRecursion(n);
+        // return ans;
+        int ans=topDown(n,dp);
         return ans;
     }
 };
